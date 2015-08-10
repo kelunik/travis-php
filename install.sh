3cfe00ad03b53b62446fd04a4cb85193353281d6
Markus Staab <maggus.staab@googlemail.com>
2015-08-10 12:32:23 +0200

    render successmessage only when commands succeed.

--- install.sh
+++ install.sh
@@ -5,17 +5,33 @@ VERSION=(`phpenv version`)
 if [[ ${VERSION[0]} != "7.0.0" && ${VERSION[0]} != "7.0" && ${VERSION[0]} != "7" ]]
 then
 	echo "aborting installation... not running with 7.0.0"
-	exit 0
+	exit 1
 fi
 
 wget http://php.kelunik.com/$1.tar.xz -q -O release.tar.xz
-echo "download successful"
+if [ $? -eq 0 ]; then
+    echo "download successful"
+else
+    echo "download failed"
+    exit 2
+fi
+
 
 tar -xJf release.tar.xz
-echo "unpacking successful"
+if [ $? -eq 0 ]; then
+    echo "unpacking successful"
+else
+    echo "unpacking failed"
+    exit 3
+fi
 
 rm release.tar.xz
-echo "deleted archive"
+if [ $? -eq 0 ]; then
+    echo "deleted archive"
+else
+    echo "deletion of archive failed"
+    exit 4
+fi
 
 mkdir -p $HOME/.phpenv/versions
 rm -rf $HOME/.phpenv/versions/$1
